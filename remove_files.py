8d0b0eae8983ca9933007db3970b2772cf994d42
KSVarun <ksvarun001@gmail.com>
2019-11-18 10:39:15 +0530

    matching both srt and vtt

--- remove_files.py
+++ remove_files.py
@@ -1,7 +1,7 @@
 import re
 import os
 
-dir_path = "/Users/idks/Downloads/MastertheCodingInterviewDataStructures+Algorithms/"
+dir_path = "/Users/idks/Downloads/[FreeTutorials.Us] Udemy - Full Stack Project Spring Boot 2.0, ReactJS, Redux"
 path = os.listdir(dir_path)
 for x in path:
     if(x == '.DS_Store'):
@@ -12,15 +12,22 @@ for file in path:
     with open(file_path, 'r') as file:
         print(file.read())"""
 
-    inside_dir_path = dir_path+file+"/"
+    inside_dir_path = dir_path+"/"+file+"/"
     path2 = os.listdir(inside_dir_path)
-    mpfiles = '.*\.srt'
+    mpfiles = '.*\(.vtt|.srt)'
+    #mpfiles_srt = '.*\.srt'
 
     for file_inside in path2:
         pattern = re.compile(mpfiles)
         matches = pattern.finditer(file_inside)
+        # pattern_srt = re.compile(mpfiles_srt)
+        # matches_srt = pattern_srt.finditer(file_inside)
 
         for match in matches:
             str_match = str(match.group(0))
             # print(inside_dir_path+str_match)
             os.remove(inside_dir_path+str_match)
+
+        # for match in matches_srt:
+        #     str_match_srt = str(match.group(0))
+        #     os.remove(inside_dir_path+str_match_srt)
